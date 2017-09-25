262b7d5cb409b94d7f0f30df7f7a88f6fead8a0e
Yonahwang <30312668+Yonahwang@users.noreply.github.com>
2017-09-25 11:55:54 +0800

    123

--- pdf-git/yonah.py
+++ pdf-git/yonah.py
@@ -2,26 +2,28 @@
 
 
 
-import subprocess
 from peepdf.PDFCore import PDFParser
-
-
+#TODO: ADD function
 
 
 def getpdfhear():
     pass
 
-
 if __name__ == '__main__':
 
-    #cmd ='python peepdf/peepdf.py /Users/fengjiaowang/Desktop/yonah/cve2010-2883.pdf'
-    #subprocess.call(cmd,shell=True)
+    # cmd = 'python peepdf/peepdf.py /Users/fengjiaowang/Desktop/yonah/cve2010-2883.pdf'
+    # subprocess.call(cmd,shell=True)
     pdfParser = PDFParser()
-    ret, pdf = pdfParser.parse('/Users/fengjiaowang/Desktop/yonah/cve2010-2883.pdf')
+    _, pdf = pdfParser.parse('/Users/fengjiaowang/Desktop/yonah/cve2010-2883.pdf')
     statsDict = pdf.getStats
     tree = pdf.getTree()
-    print tree
-    print tree.get('/Catalog')
+    version = pdf.getVersion()
+    MD5 = pdf.getMD5()
+    print"MD5:",MD5
+    print "Version:",version
+    print "Tree:",tree
+    #print tree.get('/Catalog')
+
 
     #print pdf.getObject(10).getValue()
     #a = input()
