b78c8271665256b220246b114df299d688b365b1
Yonahwang <30312668+Yonahwang@users.noreply.github.com>
2017-09-29 18:16:17 +0800

    update9

---
+++ pdf-git/featureEX.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+
+
+def fakeFile_check(filePath):
+    try:
+        from peepdf.PDFCore import PDFParser
+        pdfParser = PDFParser()
+        _, pdf = pdfParser.parse(filePath)
+        return pdf
+    except Exception:
+        return None
+
+
+def feature_extract(pdf):  # 对输入文件进行特征提取
+
+    '''***********************'''
+
+    feature = dict()
+    statsDict = pdf.getStats
+    name = pdf.getFileName()
+    md5 = pdf.getMD5()
+    for g in range(len(md5)):
+        feature['md5_' + str(g)] = int(md5[g], 16)
+
+    sha1 = pdf.getSHA1()
+    for h in range(len(sha1)):
+        feature['sha1_' + str(h)] = int(sha1[h], 16)
+    sha256 = pdf.getSHA256()
+    for s in range(len(sha256)):
+        feature['sha256_' +str(s) ] = int(sha256[s],16)
+    version = pdf.getVersion()
+    feature['ver'] = float(version)
+    feature['numstream'] = pdf.numStreams
+    feature['size'] = pdf.getSize()
+    feature['numofobject'] = pdf.numObjects
+    feature['update'] = pdf.getNumUpdates()
+    feature['comments'] = len(pdf.comments)
+    feature['error'] = len(pdf.errors)
+
+
+    print('fileneme: %s'%name)
+
+    return [feature[k] for k in feature]
