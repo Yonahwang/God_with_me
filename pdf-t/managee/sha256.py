397ee753446c93c2b79c5b0b3098702569fe4b5f
Yonahwang <634272867@qq.com>
2018-02-28 05:11:06 -0800

    sha256

--- pdf-t/managee/sha256.py
+++ pdf-t/managee/sha256.py
@@ -8,6 +8,7 @@ import pickle
 files =r"/home/yonah/PDFdata/pdfnormal"
 pathDir =  os.listdir(files)
 
+#downf = open("tsha_pdf_83442.txt",'wb')
 file_sha256 = []
 for file in pathDir:
     f = files + "/" + file
@@ -15,5 +16,9 @@ for file in pathDir:
     sh = hashlib.sha256()
     sh.update(fr.read())
     file_sha256.append(sh.hexdigest())
+    #downf.write(sh.hexdigest() + '\n')
 
 pickle.dump(file_sha256,open('pdf_sha256.pkl','wb'))
+
+#downf.close()
+
