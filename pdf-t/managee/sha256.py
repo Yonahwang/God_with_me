e911ad1cef01350327b8c87b271378a34fc706d6
Yonahwang <634272867@qq.com>
2018-02-27 22:39:11 -0800

    sss

--- pdf-t/managee/sha256.py
+++ pdf-t/managee/sha256.py
@@ -4,19 +4,17 @@
 import hashlib
 import os
 
-#file = open('F:\PDFdata\data2000\VirusS\VirusShare_0a4ad7052d9e9206303f6791d691f16d', 'rb')
-files =r"F:\PDFdata\data2000\VirusS"
-
-#sh = hashlib.sha256()
-#sh.update(f.read())
-#print sh.hexdigest()
-
+files =r"/home/yonah/PDFdata/pdfnormal"
 pathDir =  os.listdir(files)
 
-for file in pathDir:
-    f = files + file
-    fr = open(f , 'rb')
-    sh = hashlib.sha256()
-    sh.update(f.read())
-    print sh.hexdigest()
+downf = open("sha2.txt",'wb')
 
+for file in pathDir:
+    f = files + "/" + file
+    fr = open(f, 'rb')
+    sh = hashlib.sha256()
+    sh.update(fr.read())
+    print sh.hexdigest()
+    downf.write(sh.hexdigest())
+
+downf.close()
