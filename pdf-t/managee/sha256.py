80f23d9e92f1a92568a18885f19b01cc1a5861e8
Yonahwang <634272867@qq.com>
2018-02-28 05:02:06 -0800

    sha256.py

--- pdf-t/managee/sha256.py
+++ pdf-t/managee/sha256.py
@@ -3,18 +3,17 @@
 
 import hashlib
 import os
+import pickle
 
 files =r"/home/yonah/PDFdata/pdfnormal"
 pathDir =  os.listdir(files)
 
-downf = open("sha2.txt",'wb')
-
+file_sha256 = []
 for file in pathDir:
     f = files + "/" + file
     fr = open(f, 'rb')
     sh = hashlib.sha256()
     sh.update(fr.read())
-    print sh.hexdigest()
-    downf.write(sh.hexdigest())
+    file_sha256.append(sh.hexdigest())
 
-downf.close()
+pickle.dump(file_sha256,open('pdf_sha256.pkl','wb'))
