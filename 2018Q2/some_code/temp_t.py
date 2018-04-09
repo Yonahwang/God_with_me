9582abaf5542c5cc80389fd7ccf41911c238492e
Yonahwang <634272867@qq.com>
2018-04-09 16:25:41 +0800

    A9

--- 2018Q2/some_code/temp_t.py
+++ 2018Q2/some_code/temp_t.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 
-L = ['<a href="/cgi-bin/cvename.cgi?name=CVE-2004-0194">CVE-2004-0194</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2003-0508">CVE-2003-0508</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2003-0434">CVE-2003-0434</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2003-0284">CVE-2003-0284</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2003-0204">CVE-2003-0204</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2003-0142">CVE-2003-0142</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2002-1569">CVE-2002-1569</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2002-0838">CVE-2002-0838</a>', '<a href="/cgi-bin/cvename.cgi?name=CVE-2002-0030">CVE-2002-0030</a>']
-
-timm= []
-for i in L :
-
-    tim = i[-13:-9]
-    timm.append(tim)
-
-from collections import Counter
-print Counter(timm)
+for file in os.listdir('/home/yonah/PDFdata/mal'):
+    if file[-4: ] != '.pdf':
+        new_name = file + '.pdf'
+        os.rename('/home/yonah/PDFdata/mal/'+ file, '/home/yonah/PDFdata/mal/' + new_name)
 
 
 
