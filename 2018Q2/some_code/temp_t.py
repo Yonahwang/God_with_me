1dce429df9e8334d762fc98db9dbe4d58c61e082
Yonahwang <634272867@qq.com>
2018-04-10 20:28:18 -0700

    model3

--- 2018Q2/some_code/temp_t.py
+++ 2018Q2/some_code/temp_t.py
@@ -30,41 +30,47 @@ def data_clear(file):
         else:
             y.append(0)'''
 
-    #print y
-    X = file[['author_dot', 'author_lc', 'author_len', 'author_mismatch', 'author_num', 'author_oth', 'author_uc',
-                'box_nonother_types', 'box_other_only', 'company_mismatch', 'count_acroform', 'count_acroform_obs',
-                'count_action', 'count_action_obs', 'count_box_a4', 'count_box_legal', 'count_box_letter',
-                'count_box_other', 'count_box_overlap', 'count_endobj', 'count_endstream', 'count_eof', 'count_font',
-                'count_font_obs', 'count_image_large', 'count_image_med', 'count_image_small', 'count_image_total',
-                'count_image_xlarge', 'count_image_xsmall', 'count_javascript', 'count_javascript_obs', 'count_js',
-                'count_js_obs', 'count_obj', 'count_objstm', 'count_objstm_obs', 'count_page', 'count_page_obs',
-                'count_startxref', 'count_stream', 'count_stream_diff', 'count_trailer', 'count_xref', 'createdate_dot',
-                'createdate_mismatch', 'createdate_ts', 'createdate_tz', 'createdate_version_ratio', 'creator_dot',
-                'creator_lc', 'creator_len', 'creator_mismatch', 'creator_num', 'creator_oth', 'creator_uc', 'delta_ts',
-                'delta_tz', 'image_mismatch', 'image_totalpx', 'keywords_dot', 'keywords_lc', 'keywords_len',
-                'keywords_mismatch', 'keywords_num', 'keywords_oth', 'keywords_uc', 'len_obj_avg', 'len_obj_max',
-                'len_obj_min', 'len_stream_avg', 'len_stream_max', 'len_stream_min', 'moddate_dot', 'moddate_mismatch',
-                'moddate_ts', 'moddate_tz', 'moddate_version_ratio', 'pdfid0_dot', 'pdfid0_lc', 'pdfid0_len',
-                'pdfid0_mismatch', 'pdfid0_num', 'pdfid0_oth', 'pdfid0_uc', 'pdfid1_dot', 'pdfid1_lc', 'pdfid1_len',
-                'pdfid1_mismatch', 'pdfid1_num', 'pdfid1_oth', 'pdfid1_uc', 'pdfid_mismatch', 'pos_acroform_avg',
-                'pos_acroform_max', 'pos_acroform_min', 'pos_box_avg', 'pos_box_max', 'pos_box_min', 'pos_eof_avg',
-                'pos_eof_max', 'pos_eof_min', 'pos_image_avg', 'pos_image_max', 'pos_image_min', 'pos_page_avg',
-                'pos_page_max', 'pos_page_min', 'producer_dot', 'producer_lc', 'producer_len', 'producer_mismatch',
-                'producer_num', 'producer_oth', 'producer_uc', 'ratio_imagepx_size', 'ratio_size_obj',
-                'ratio_size_page', 'ratio_size_stream', 'size', 'subject_dot', 'subject_lc', 'subject_len',
-                'subject_mismatch', 'subject_num', 'subject_oth', 'subject_uc', 'title_dot', 'title_lc', 'title_len',
-                'title_mismatch', 'title_num', 'title_oth', 'title_uc', 'version']]
+    X1 = file[['author_dot', 'author_lc', 'author_len', 'author_mismatch', 'author_num', 'author_oth', 'author_uc','keywords_dot',
+               'keywords_lc', 'keywords_len','keywords_mismatch', 'keywords_num', 'keywords_oth', 'keywords_uc','moddate_dot', 'moddate_mismatch',
+               'moddate_ts', 'moddate_tz', 'moddate_version_ratio','producer_dot', 'producer_lc', 'producer_len', 'producer_mismatch','producer_num',
+               'producer_oth', 'producer_uc']]
 
-    XX = np.array(X)
+    X2 = file[['box_nonother_types', 'box_other_only','count_box_other', 'count_box_overlap','count_box_a4', 'count_box_legal',
+               'count_box_letter','count_startxref', 'count_stream', 'count_stream_diff', 'count_trailer', 'count_xref','pos_acroform_max',
+               'pos_acroform_min', 'pos_box_avg', 'pos_box_max', 'pos_box_min', 'pos_eof_avg','pos_eof_max', 'pos_eof_min', 'pos_image_avg',
+               'pos_image_max', 'pos_image_min', 'pos_page_avg','pos_page_max', 'pos_page_min','pos_acroform_avg']]
+
+    X3 = file[['count_action', 'count_action_obs','count_javascript', 'count_javascript_obs', 'count_js','count_js_obs', 'count_obj',
+               'count_objstm', 'count_objstm_obs','ratio_size_obj','ratio_size_page', 'ratio_size_stream', 'size', 'subject_dot', 'subject_lc',
+               'subject_len']]
+
+    X4 = file[['count_endobj', 'count_endstream', 'count_eof', 'count_font','count_font_obs','len_obj_avg', 'len_obj_max','len_obj_min',
+               'len_stream_avg', 'len_stream_max', 'len_stream_min','pdfid0_dot', 'pdfid0_lc', 'pdfid0_len','pdfid0_mismatch', 'pdfid0_num',
+               'pdfid0_oth', 'pdfid0_uc', 'pdfid1_dot', 'pdfid1_lc', 'pdfid1_len','pdfid1_mismatch', 'pdfid1_num', 'pdfid1_oth', 'pdfid1_uc',
+               'pdfid_mismatch']]
+
+    X5 = file[['count_image_large', 'count_image_med', 'count_image_small', 'count_image_total','count_image_xlarge', 'count_image_xsmall',
+               'image_mismatch', 'image_totalpx','ratio_imagepx_size','company_mismatch', 'count_acroform', 'count_acroform_obs','count_page',
+               'count_page_obs','delta_ts','delta_tz']]
+
+    X6 = file[['createdate_dot','createdate_mismatch', 'createdate_ts', 'createdate_tz', 'createdate_version_ratio', 'creator_dot',
+               'creator_lc', 'creator_len', 'creator_mismatch', 'creator_num', 'creator_oth', 'creator_uc', 'subject_mismatch', 'subject_num',
+               'subject_oth', 'subject_uc', 'title_dot', 'title_lc', 'title_len','title_mismatch', 'title_num', 'title_oth', 'title_uc', 'version']]
+
+    X7 = file[['count_acroform_obs', 'count_box_a4', 'count_box_legal', 'count_box_letter', 'count_box_other', 'count_box_overlap', 'count_endobj',
+               'count_endstream', 'count_font', 'count_image_large', 'count_image_med', 'count_image_small', 'count_image_total','count_image_xlarge',
+               'count_image_xsmall', 'count_js', 'count_obj', 'count_objstm', 'count_page_obs', 'count_startxref', 'count_stream', 'title_len', 'creator_len']]
+
+
+    XX = np.array(X7)
     Xint = XX.astype(int)
 
+    return y, Xint.tolist()
 
-
-    return y,Xint.tolist()
 
 label,feature = data_clear(f_tarin)
 
-pca = decomposition.PCA(n_components=12)
+pca = decomposition.PCA(n_components=2)
 newX=pca.fit_transform(feature)
 
 from sklearn import preprocessing
@@ -72,7 +78,7 @@ min_max_scaler = preprocessing.MinMaxScaler()
 newX = min_max_scaler.fit_transform(newX)
 
 from pandas import DataFrame
-data = DataFrame(newX,columns=list('a''b''c''d''f''g''h''u''r''y''o''p'))
+data = DataFrame(newX,columns=list('a''b'))
 data['label'] = label
 
 ax = plt.subplot(231)
@@ -82,59 +88,21 @@ y_values = data.loc[data['label']==0].b.tolist()
 x1_values = data.loc[data['label']==1].a.tolist()
 y1_values = data.loc[data['label']==1].b.tolist()
 
+
 plt.scatter(x_values, y_values, c = 'g',marker = '.')
 plt.scatter(x1_values, y1_values, c = 'r',marker = '^')
-
-
-ax2 = plt.subplot(232)
-x_values = data.loc[data['label']==0].c.tolist()
-y_values = data.loc[data['label']==0].d.tolist()
-
-x1_values = data.loc[data['label']==1].c.tolist()
-y1_values = data.loc[data['label']==1].d.tolist()
-plt.scatter(x_values, y_values, c = 'g',marker = '.')
-plt.scatter(x1_values, y1_values, c = 'r',marker = '^')
-
-ax3 = plt.subplot(233)
-x_values = data.loc[data['label']==0].f.tolist()
-y_values = data.loc[data['label']==0].g.tolist()
-
-x1_values = data.loc[data['label']==1].f.tolist()
-y1_values = data.loc[data['label']==1].g.tolist()
-plt.scatter(x_values, y_values, c = 'g',marker = '.')
-plt.scatter(x1_values, y1_values, c = 'r',marker = '^')
-
-ax4 = plt.subplot(234)
-x_values = data.loc[data['label']==0].h.tolist()
-y_values = data.loc[data['label']==0].u.tolist()
-
-x1_values = data.loc[data['label']==1].h.tolist()
-y1_values = data.loc[data['label']==1].u.tolist()
-plt.scatter(x_values, y_values, c = 'g',marker = '.')
-plt.scatter(x1_values, y1_values, c = 'r',marker = '^')
-
-ax4 = plt.subplot(235)
-x_values = data.loc[data['label']==0].r.tolist()
-y_values = data.loc[data['label']==0].y.tolist()
-
-x1_values = data.loc[data['label']==1].r.tolist()
-y1_values = data.loc[data['label']==1].y.tolist()
-plt.scatter(x_values, y_values, c = 'g',marker = '.')
-plt.scatter(x1_values, y1_values, c = 'r',marker = '^')
-
-ax4 = plt.subplot(236)
-x_values = data.loc[data['label']==0].o.tolist()
-y_values = data.loc[data['label']==0].p.tolist()
-
-x1_values = data.loc[data['label']==1].o.tolist()
-y1_values = data.loc[data['label']==1].p.tolist()
-plt.scatter(x_values, y_values, c = 'g',marker = '.')
-plt.scatter(x1_values, y1_values, c = 'r',marker = '^')
+# 设置图表标题并给坐标轴加上标签
+plt.title('File distribution', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
 
 
 # 设置刻度标记的大小
 plt.tick_params(axis='both', which='major', labelsize=14)
 plt.show()
+
+
+
 
 
 
