53b8dcea93440b74d011750bfcf15b772790ba0d
Yonahwang <634272867@qq.com>
2018-05-22 05:29:53 -0700

    ver_plot

--- 2018Q2/plot_set/file_version.py
+++ 2018Q2/plot_set/file_version.py
@@ -4,14 +4,27 @@
 
 import numpy as np
 import pandas as pd
-import datetime
-
+import matplotlib.pyplot as plt
 
 f_tarin = pd.read_csv('/home/yonah/God_with_me/2018Q2/MLmodel3/example/test4K.csv')
 
 
 def get_version(file):
     ver = file['version']
-    print ver
+    return ver.tolist()
+ver = get_version(f_tarin)
+#ver = [4, 3, 5, 4, 3, 4, 3, 4, 3, 2, 5, 5, 4, 6, 5, 5, 5, 4, 6, 6, 4, 5, 4, 7, 4, 6, 5, 5, 5, 5, 7, 5, 4, 6, 4, 7, 4, 4, 4, 4, 4, 5, 5, 3, 5, 5, 6, 5, 4, 5, 4, 6, 7, 2, 6, 3, 5, 6, 5, 2, 4, 5, 7, 5]
+Se_ver = pd.value_counts(ver)
+labels =  Se_ver.index._data
+values =  Se_ver.values
 
-get_version(f_tarin)
+fig = plt.figure()
+plt.pie(values, labels=labels, autopct='%1.2f%%')  # 画饼图（数据，数据对应的标签，百分数保留两位小数点）
+plt.title("Version distribution")
+
+plt.show()
+
+
+
+
+#
